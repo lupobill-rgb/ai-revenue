1d2e47e2d4b219b73790e526109bce00b76fac02
Bill Lupo <lupo.bill@gmail.com>
2026-02-03 13:07:24 -0600

    Remove temporary PowerShell scripts for finding non-ASCII characters and fixing BOM in the .env file.

--- .tmp-fix-bom.ps1
+++
@@ -1,7 +0,0 @@
-$bytes = [IO.File]::ReadAllBytes('.env')
-$text = [System.Text.Encoding]::UTF8.GetString($bytes)
-if ($text.Length -gt 0 -and $text[0] -eq [char]0xFEFF) {
-  $text = $text.Substring(1)
-}
-$utf8NoBom = New-Object System.Text.UTF8Encoding($false)
-[IO.File]::WriteAllText('.env', $text, $utf8NoBom)
